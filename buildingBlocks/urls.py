2f0120fcf252706ac90babcb2af8bd6334d6678c
Izzatbek <izzatsultanov77@gmail.com>
2024-07-20 20:40:19 +0500

    added swagger

--- buildingBlocks/urls.py
+++ buildingBlocks/urls.py
@@ -4,8 +4,24 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from django.urls import path, re_path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Custom Pc",
+      default_version='v1',
+      description="Test description",
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('orders',views.OrdersView.as_view(), name='orders'),
     path('orders/create', views.CreateOrderView.as_view(), name='create-order'),
     path('orders/<int:pk>',views.OrdersDetailView.as_view(), name='order-detail'),
--- pcbuilder/settings.py
+++ pcbuilder/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'buildingBlocks',
     'rest_framework',
     'rest_framework_simplejwt',
+    'drf_yasg'
 ]
 
 REST_FRAMEWORK = {
